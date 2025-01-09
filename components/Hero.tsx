0c5b2e1ef45fda09cf42c57fd99a59a2a557c228
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2025-01-09 16:47:59 +0100

    ✨add bouton component and styling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./ui/MagicButton";
 
 const Hero = () => {
   return (
@@ -32,6 +33,9 @@ const Hero = () => {
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
             Hi, I&apos;m Chris, a Full-Stack developer based in France.
           </p>
+          <a href="#about">
+            <MagicButton title="Show my work" />
+          </a>
         </div>
       </div>
     </div>
