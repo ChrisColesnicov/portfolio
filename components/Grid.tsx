943fc1935f8a16234b45608c2d75032fa00d164e
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2025-01-12 19:45:06 +0100

    grid initial setup done

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -5,7 +5,7 @@ const Grid = () => {
   return (
     <section id="about">
       <BentoGrid>
-        {[{ title: "Title1", description: "Desc1", id: "1" }].map((item) => (
+        {[{ title: "Title1", description: "Desc1", id: 1 }].map((item) => (
           <BentoGridItem
             id={item.id}
             key={item.id}
