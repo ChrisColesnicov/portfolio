b9a1449560d6971cd24fec3bd6cf8536bf7783be
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2025-01-21 15:32:52 +0100

    position fix for circles

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -8,7 +8,7 @@ export const HeroOrbit = ({
   return (
     <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 ">
       <div
-        className="rotate-45"
+        className="flex items-start justify-start"
         style={{
           transform: `rotate(${rotation}deg)`,
           height: `${size}px`,
@@ -16,7 +16,7 @@ export const HeroOrbit = ({
         }}
       >
         <div className="inline-flex" style={{
-            transform: `rotate(${rotation * -1})`
+            transform: `rotate(${rotation * -1}deg)`
         }}>{children}</div>
       </div>
     </div>
