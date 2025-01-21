7841a946c40a0d98a52cabcb60abc18d947060de
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2025-01-21 15:11:17 +0100

    styling alignement for stars and rotation addition to hero orbit

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -3,17 +3,21 @@ import { PropsWithChildren } from "react";
 export const HeroOrbit = ({
   children,
   size,
-}: PropsWithChildren<{ size: number }>) => {
+  rotation,
+}: PropsWithChildren<{ size: number; rotation: number }>) => {
   return (
     <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 ">
       <div
-        className="border border-red-500"
+        className="rotate-45"
         style={{
+          transform: `rotate(${rotation}deg)`,
           height: `${size}px`,
           width: `${size}px`,
         }}
       >
-        <div className="border border-red-500 inline-flex">{children}</div>
+        <div className="inline-flex" style={{
+            transform: `rotate(${rotation * -1})`
+        }}>{children}</div>
       </div>
     </div>
   );
