1d06a44dcc0f98047c5a65a87e315d87e509b2e4
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2025-01-22 12:31:31 +0100

    mobile view for testimonials with styling, top margin removal for text

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -50,27 +50,29 @@ export const TestimonialsSection = () => {
           title="What Clients (Will) Say About Me"
           description="This could be your review here—let’s make it happen!"
         />
-        <div>
-          {testimonials.map((testimonial) => (
-            <Card key={testimonial.name}>
-              <div className="flex gap-4 items-center">
-                <div className="size-14 bg-gray-700 inline-flex rounded-full items-center justify-center">
-                  <Image
-                    src={testimonial.avatar}
-                    alt={testimonial.name}
-                    className="max-h-full"
-                  />
-                </div>
-                <div>
-                  <div className="font-semibold">{testimonial.name}</div>
-                  <div className="text-sm text-white/40">
-                    {testimonial.position}
+        <div className="mt-16 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+          <div className="flex flex-none gap-8">
+            {testimonials.map((testimonial) => (
+              <Card key={testimonial.name} className="max-w-xs">
+                <div className="flex gap-4 items-center">
+                  <div className="size-14 bg-gray-700 inline-flex rounded-full items-center justify-center flex-shrink-0">
+                    <Image
+                      src={testimonial.avatar}
+                      alt={testimonial.name}
+                      className="max-h-full"
+                    />
+                  </div>
+                  <div>
+                    <div className="font-semibold">{testimonial.name}</div>
+                    <div className="text-sm text-white/40">
+                      {testimonial.position}
+                    </div>
                   </div>
                 </div>
-              </div>
-              <p className="mt-4 text-sm">{testimonial.text}</p>
-            </Card>
-          ))}
+                <p className="text-sm">{testimonial.text}</p>
+              </Card>
+            ))}
+          </div>
         </div>
       </div>
     </div>
