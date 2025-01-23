0c53d33e6bb2e41c74200384a61cc999a1243b75
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2025-01-23 17:16:40 +0100

    🎨 styling with grain img and bg gradient, mobile version done

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,12 +1,30 @@
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+import grainImage from "@/assets/images/grain.jpg";
+
 export const ContactSection = () => {
   return (
-    <div>
-      <h2>Let’s build something extraordinary together</h2>
-      <p>
-        Got a project in mind? Let’s connect and see how we can bring your
-        vision to life and help you reach your goals.
-      </p>
-      <button>Get in touch</button>
+    <div className="py-16 pt-12">
+      <div className="container">
+        <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center relative overflow-hidden z-0">
+          <div
+            className="absolute inset-0 opacity-5 -z-10"
+            style={{
+              backgroundImage: `url(${grainImage.src})`,
+            }}
+          ></div>
+          <h2 className="font-serif text-2xl">
+            Let’s build something extraordinary together
+          </h2>
+          <p className="text-sm mt-2">
+            Got a project in mind? Let’s connect and see how we can bring your
+            vision to life and help you reach your goals.
+          </p>
+          <button className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 mt-8">
+            <span className="font-semibold">Get In Touch</span>
+            <ArrowUpRightIcon className="size-4" />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
