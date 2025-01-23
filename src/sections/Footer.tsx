ed1b9f7a4284eec7ddb7c3f4634ae3d281a009cc
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2025-01-23 17:44:09 +0100

    bottom gradient styling adjustment

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -13,13 +13,18 @@ const footerLinks = [
 
 export const Footer = () => {
   return (
-    <footer>
+    <footer className="relative z-0 overflow-x-clip">
+      <div className="absolute h-[200px] w-[1200px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-20"></div>
       <div className="container">
         <div className="border-t border-white/50 py-6 text-sm flex flex-col items-center gap-8">
           <div className="text-white/40">&copy; 2025. All rights reserved.</div>
           <nav className="flex flex-col items-center gap-8">
             {footerLinks.map((link) => (
-              <a href="#" key={link.title} className="inline-flex items-center gap-1.5">
+              <a
+                href={link.href}
+                key={link.title}
+                className="inline-flex items-center gap-1.5"
+              >
                 <span className="font-semibold">{link.title}</span>
                 <ArrowUpRightIcon className="size-4" />
               </a>
