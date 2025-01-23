a73eb6f0e891b53c7f05a2c79c4c4e8a4c1a6865
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2025-01-23 17:07:54 +0100

    ✨ contact section initiated with text and static elements

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,3 +1,12 @@
 export const ContactSection = () => {
-  return <div>Contact Section</div>;
+  return (
+    <div>
+      <h2>Let’s build something extraordinary together</h2>
+      <p>
+        Got a project in mind? Let’s connect and see how we can bring your
+        vision to life and help you reach your goals.
+      </p>
+      <button>Get in touch</button>
+    </div>
+  );
 };
