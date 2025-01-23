0ce8327857e31e13ab5edd7f92872e96dd6ca2be
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2025-01-23 17:34:18 +0100

    content included with links and copyright

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,32 @@
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+
+const footerLinks = [
+  {
+    title: "LinkedIn",
+    href: "#",
+  },
+  {
+    title: "Github",
+    href: "#",
+  },
+];
+
 export const Footer = () => {
-  return <div>Footer Section</div>;
+  return (
+    <footer>
+      <div className="container">
+        <div>
+          <div>&copy; 2025. All rights reserved.</div>
+          <nav>
+            {footerLinks.map((link) => (
+              <a href="#" key={link.title}>
+                <span>{link.title}</span>
+                <ArrowUpRightIcon />
+              </a>
+            ))}
+          </nav>
+        </div>
+      </div>
+    </footer>
+  );
 };
