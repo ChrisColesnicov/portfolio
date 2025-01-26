4c3872355e152ba2404a38ea6d41073d8677270a
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2025-01-26 21:24:48 +0100

    text button change

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -89,7 +89,7 @@ export const ProjectsSection = () => {
                   </ul>
                   <a href={project.link}>
                     <button className="bg-white/50 text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
-                      <span>Available soon</span>
+                      <span>Demo Available Soon</span>
                       <ArrowUpRightIcon className="size-4" />
                     </button>
                   </a>
