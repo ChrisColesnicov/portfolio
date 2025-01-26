3dd8afc34977daa2bb0d5b1155f1daefdad5f785
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2025-01-26 21:20:59 +0100

    resume in footer with gdrive link

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -9,12 +9,16 @@ const footerLinks = [
     title: "Github",
     href: "https://github.com/ChrisColesnicov",
   },
+  {
+    title: "Resume (PDF)",
+    href: "https://drive.google.com/file/d/1xF3xUMgffoUOcaXA2I10hPJbj1PT4OyF/view?usp=sharing",
+  },
 ];
 
 export const Footer = () => {
   return (
     <footer className="relative z-0 overflow-x-clip">
-      <div className="absolute h-[200px] md:h-[150px] w-[1200px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-20"></div>
+      <div className="absolute h-[300px] md:h-[150px] w-[1200px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-20"></div>
       <div className="container">
         <div className="border-t border-white/50 py-6 text-sm flex flex-col md:flex-row md:justify-between items-center gap-8">
           <div className="text-white/40">&copy; 2025. All rights reserved.</div>
